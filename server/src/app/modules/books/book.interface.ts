52e1beec32307b9ab31858ef6582d4c2c8fa8103
anamolhasan <mdanamolhasan1998@gmail.com>
2025-12-15 01:06:15 +0600

    schema lering

---
+++ server/src/app/modules/books/book.interface.ts
@@ -0,0 +1,17 @@
+//  type definition
+
+import { Document } from "mongoose";
+
+
+export interface IBook extends Document{
+    title:string;
+    author:string;
+    description:string;
+    genre:string;
+    publicationYear:number,
+    isbn:string;
+    price:number;
+    isAvailable:boolean;
+    createdAt:Date;
+    updateAt:Date;
+}
